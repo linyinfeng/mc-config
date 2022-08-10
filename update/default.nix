9c8f9333f10c7c816c91f4be757956dca931b00e
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-08-11 00:12:07 +0800

    Apply nixfmt

--- update/default.nix
+++ update/default.nix
@@ -1,11 +1,6 @@
 { writeShellScriptBin, python3 }:
 
-let
-  pyEnv = python3.withPackages (p: with p; [
-    iso8601
-    requests
-  ]);
-in
-writeShellScriptBin "update" ''
+let pyEnv = python3.withPackages (p: with p; [ iso8601 requests ]);
+in writeShellScriptBin "update" ''
   ${pyEnv}/bin/python ${./update.py} "$@"
 ''
