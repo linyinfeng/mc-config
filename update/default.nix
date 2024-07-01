62d16f2f6f76e46177131c099d7e3941c09b8826
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-07-02 02:07:54 +0800

    Use exec in update

--- update/default.nix
+++ update/default.nix
@@ -6,5 +6,5 @@
   pyEnv = python3.withPackages (p: with p; [iso8601 requests natsort]);
 in
   writeShellScriptBin "update" ''
-    ${pyEnv}/bin/python ${./update.py} --game-manifests "${minecraft-json}/vanilla/manifests.json" "$@"
+    exec ${pyEnv}/bin/python ${./update.py} --game-manifests "${minecraft-json}/vanilla/manifests.json" "$@"
   ''
